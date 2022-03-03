b6dd6af16fecb54d48492dea91721e3e806e7092
Miracle Yin <miracleyin@live.com>
2022-03-04 00:15:36 +0800

    nvim macOS update

--- link_config.sh
+++ link_config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Miracleyin 2022.3.4
 
 script_dir=$(cd $(dirname $0);pwd)
 echo $script_dir
 
 ln -sf $script_dir/alacritty $HOME/.config/alacritty
+ln -sf $script_dir/nvim $HOME/.config/nvim
+
