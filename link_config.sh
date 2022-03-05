0cc3db76495f4b3b0b7a0ef29f60554e7d62371e
MIracleyin <miracleyin@live.com>
2022-03-05 22:42:52 +0800

    add dotbot

--- link_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Miracleyin 2022.3.4
-
-script_dir=$(cd $(dirname $0);pwd)
-echo $script_dir
-
-ln -sf $script_dir/alacritty $HOME/.config/alacritty
-ln -sf $script_dir/nvim $HOME/.config/nvim
-
